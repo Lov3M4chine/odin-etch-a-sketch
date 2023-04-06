84065ce9514e58e7cb880b5bba39065a6b2e0bfc
Lov3M4chine <125694577+Lov3M4chine@users.noreply.github.com>
2023-04-06 10:02:30 -0600

    Establish function that changes grid color on click or mousedrag

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 let dimensions = 16;
+let isMouseDown = false;
 const gridContainer = document.querySelector('.grid-container');
 
 
 //establish starting grid
 function startingGrid () {
-    gridContainer.style.gridTemplateColumns = 'repeat(${dimensions}, 1fr)';
+    gridContainer.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
 
     for (let i = 0; i < dimensions; i++) {
@@ -18,6 +19,30 @@ function startingGrid () {
 
 startingGrid ();
 //change grid colors when mouse is dragged over
+function changeGridColor () {
+    document.addEventListener('mousedown', function () {
+        isMouseDown = true;
+    });
+    document.addEventListener('mouseup', function () {
+        isMouseDown = false;
+    });
+    document.addEventListener('mousemove', function (e) {
+        if (isMouseDown) {
+            const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+            if (hoveredElement && hoveredElement.classList.contains('grid-block')) {
+                hoveredElement.classList.add('activated');
+            }
+        }
+    });
+    document.addEventListener('click', function(e) {
+        const element = e.target;
+        if (element && element.classList.contains('grid-block')) {
+            element.classList.add('activated');
+        }
+    })
+}
+
+changeGridColor ();
 //clear button should clear sketch
 //grid button should ask how big grid should be
 //user-defined grid should be established
